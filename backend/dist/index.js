0a8d5d7c944dcdb3c8e5205ae187fa673b18a98e
Warm Ronaldo Dude <loliconneko@gmail.com>
2021-12-20 13:14:43 +0700

    delete files use only in local env

--- backend/dist/index.js
+++
@@ -1,8 +0,0 @@
-var _bootup = require("./graphql/bootup");
-const port = 8080;
-(async ()=>{
-    await (0, _bootup).start({
-        port
-    });
-    console.info(`GQL start at port ${port}`);
-})();
